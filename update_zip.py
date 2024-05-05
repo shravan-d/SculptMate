c74e4fa4319fc28f1387cc4ea9fcea0acdb27acd
shravan-d <shravandinakaran@gmail.com>
2024-05-05 12:25:56 -0400

    Added quick add-on update script

---
+++ update_zip.py
@@ -0,0 +1,24 @@
+import zipfile
+import os
+
+dirs_to_disregard = [".git", "assets", ".github", "checkpoints", "__pycache__"]
+
+def update_zip(zip_file, folder_path):
+    with zipfile.ZipFile(zip_file, 'a') as zipf:
+        for root, _, files in os.walk(folder_path):
+            disregard = False
+            for dir in dirs_to_disregard:
+                if dir in root:
+                    disregard = True
+                    break
+            if disregard:
+                continue
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = "SculptMate/"+os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname=arcname)
+
+
+folder_path = '../SculptMate/'
+zip_file = '../SculptMate.zip'
+update_zip(zip_file, folder_path)
